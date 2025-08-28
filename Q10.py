0c3870669f0b792f43ee87749ed5a336210a8bfe
TanmayDawande <tdawande007@gmail.com>
2025-08-28 13:15:23 +0530

    added q10

---
+++ Q10.py
@@ -0,0 +1,25 @@
+n = int(input())
+k = int(input())
+lst = []
+
+for i in range(n-1):
+    lst.append(int(input()))
+    
+for j in range(1, n-2, 1):
+    
+    for k in range(n-2):
+        if lst[j] == lst[j+1]:
+            print("No peak")
+            break
+    
+        if (lst[j] > lst[j-1]) and (lst[j] > lst[j+1]):
+            print(lst[j])
+            
+for l in range(n-1):
+    temp = lst[l]
+    for m in range(n-1):
+        if temp==lst[m]:
+            
+    
+        
+        
